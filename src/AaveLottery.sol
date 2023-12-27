e42aa52fd46d935a2608aedb915f05b8893f9781
Paul Shpilsher <paul.shpilsher@gmail.com>
2023-12-27 17:15:56 +0300

    add dev version od random number generator

--- src/AaveLottery.sol
+++ src/AaveLottery.sol
@@ -3,10 +3,10 @@ pragma solidity ^0.8.13;
 
 contract AaveLottery {
     struct Round {
-        uint256 endTime;    // End time of round
+        uint256 endTime; // End time of round
         uint256 totalStake; // Total amount of ETH staked from, all users
-        uint256 award;      // Jackpot amount
-        address winner;     // Winner of round
+        uint256 award; // Jackpot amount
+        address winner; // Winner of round
     }
 
     struct Ticket {
@@ -15,6 +15,9 @@ contract AaveLottery {
 
     // Duration of each round in seconds
     uint256 public roundDuration;
+
+    // Current round id
+    uint256 public currentId;
 
     // Mapping of roundId to Round
     mapping(uint256 => Round) public rounds;
@@ -34,7 +37,10 @@ contract AaveLottery {
         return rounds[roundId];
     }
 
-    function getTicket(uint256 roundId, address user) external view returns (Ticket memory) {
+    function getTicket(
+        uint256 roundId,
+        address user
+    ) external view returns (Ticket memory) {
         // Validation
         // Return ticket
         return tickets[roundId][user];
@@ -68,5 +74,21 @@ contract AaveLottery {
         // Transfer jackpot to winner
 
         payable(msg.sender).transfer(address(this).balance);
+    }
+
+    // Random number generator
+    // Do not use in production.  TODO: Use Chainlink VRF to generate random number
+    // https://docs.chain.link/docs/get-a-random-number/
+    function random() private view returns (uint256) {
+        return
+            uint256(
+                keccak256(
+                    abi.encodePacked(
+                        block.timestamp,
+                        rounds[currentId].totalStake,
+                        currentId
+                    )
+                )
+            );
     }
 }
